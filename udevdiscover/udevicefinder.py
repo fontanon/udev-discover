9b7ac324a2b0565bee855e7a2c0863899d324554
J. Félix Ontañón <fontanon@emergya.es>
2011-01-02 17:15:28 +0100

    Rescanning of devices supported in udevicefinder Better tracking of new devices in udevicefinder

--- udevdiscover/udevicefinder.py
+++ udevdiscover/udevicefinder.py
@@ -33,14 +33,14 @@ class DeviceFinder(gobject.GObject):
     An object that will find and monitor Wiimote devices on your 
     machine and emit signals when are connected / disconnected
     '''
-    
+
     __gsignals__ = {
         'connected': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, 
             (gobject.TYPE_PYOBJECT,)),
         'disconnected': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, 
             (gobject.TYPE_PYOBJECT,)),
     }
-    
+
     def __init__(self, subsystems=['*'], parent_tree=False):
         '''
         Create a new DeviceFinder and attach to the udev system to 
@@ -53,30 +53,45 @@ class DeviceFinder(gobject.GObject):
         self.devices_tree = {}
         self.devices_list = []
 
-        def explore_parent(device, devices_tree, devices_list):
-            path = device.get_sysfs_path()        
-            parent = device.get_parent()
+        self.client.connect('uevent', self.event)
 
-            if parent:
-                explore_parent(parent, devices_tree, devices_list)
-                devices_tree[path] = self.get_device_object(device)
-            else:
-                devices_tree[path] = self.get_device_object(device)
-                
-            dev = self.get_device_object(device)
-            if not dev in devices_list:
-                devices_list.append(dev)
+    def scan_subsystems(self, subsystems=['*'], parent_tree=False):
+        self.client = gudev.Client(subsystems)
+        self.subsystems = subsystems
+        self.devices_tree = {}
+        self.devices_list = []
 
         for subsystem in subsystems:
             for device in self.client.query_by_subsystem(subsystem):
                 if parent_tree: 
-                    explore_parent(device, self.devices_tree, self.devices_list)
+                    self.__explore_parent(device, self.devices_tree, 
+                        self.devices_list)
                 else:
                     self.devices_list.append(self.get_device_object(device))
                     self.devices_tree[device.get_sysfs_path()] = \
                         self.get_device_object(device)
 
         self.client.connect('uevent', self.event)
+        self.parent_tree = parent_tree
+
+    def __explore_parent(self, device, devices_tree, devices_list):
+        path = device.get_sysfs_path()
+        parent = device.get_parent()
+        tree = []
+
+        dev = self.get_device_object(device)
+
+        if parent:
+            tree.append(self.__explore_parent(parent, devices_tree, devices_list))
+            devices_tree[path] = dev
+        else:
+            devices_tree[path] = dev
+
+        if not dev in devices_list:
+            devices_list.append(dev)
+            tree.append(dev)
+
+        return dev
 
     def get_device_object(self, device):
         #FIXME: Devices needs a creation factory pattern
@@ -87,19 +102,18 @@ class DeviceFinder(gobject.GObject):
             dev = PCIDevice(device)
         else:
             dev = Device(device)
-        
+
         return dev
 
     def get_devices_tree(self):
         return self.devices_tree
-        
+
     def get_devices(self):
         return self.devices_list
-        #return [device_tuple[0] for sysfs_path, device_tuple in self.devices.items()]
 
     def event(self, client, action, device):
         '''Handle a udev event'''
-        
+
         return {
             'add': self.device_added,
 #            'change': self.device_changed,
@@ -108,31 +122,37 @@ class DeviceFinder(gobject.GObject):
 
     def device_added(self, device, subsystem):
         '''Called when a device has been added to the system'''
-        
-        print device, subsystem
-        
-        path = device.get_sysfs_path()
-        self.devices_tree[path] = Device(device)
-        self.devices_list.append(Device(device))
-        self.emit('connected', device)
+
+        dev = self.get_device_object(device)
+        devices_founded = [dev]
+
+        if self.parent_tree: 
+            devices_tree.append(self.__explore_parent(device, self.devices_tree,
+                self.devices_list))
+        else:
+            self.devices_list.append(dev)
+            self.devices_tree[device.get_sysfs_path()] = dev
+
+        for found_device in devices_founded:
+            self.emit('connected', found_device)
 
 gobject.type_register(DeviceFinder)
 
 if __name__ == '__main__':
     import gobject
-    
+
     def found(finder, device):
         print device.path + ': ' + device.nice_label
-    
+
     def lost(finder, device):
         print device.path + ': ' + device.nice_label
-    
+
     finder = DeviceFinder()
     finder.connect('connected', found)
     finder.connect('disconnected', lost)
-    
+
     import pprint
     pprint.pprint(finder.devices_tree)
-    
+
     loop = gobject.MainLoop()
     loop.run()
